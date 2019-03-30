67134266ccaf10e42f4e244c62e7bd7475067a74
runrunlolz <runnan007@126.com>
2019-03-30 16:42:59 +0800

    finish page

--- src/store/index.js
+++ src/store/index.js
@@ -13,10 +13,5 @@ export default new Vuex.Store({
       ctx.commit('changeCity', city)
     }
   },
-  mutations,
-  getters: {
-    doubleCity (state) {
-      return state.city + ' ' + state.city
-    }
-  }
+  mutations
 })
