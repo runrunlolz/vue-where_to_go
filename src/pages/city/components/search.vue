6adebf27c182eaa654e218c0d51954f1b3420682
runrunlolz <runnan007@126.com>
2019-03-30 16:04:59 +0800

    use vuex

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -5,7 +5,7 @@
       </div>
       <div v-show="keyword" class="search-content" ref="search">
         <ul>
-          <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+          <li class="search-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)">{{item.name}}</li>
           <li v-show="hasNoData" class="search-item border-bottom">没有匹配数据</li>
         </ul>
       </div>
@@ -14,6 +14,7 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import {mapMutations} from 'vuex'
 export default {
   name: 'CitySearch',
   props: {
@@ -25,6 +26,13 @@ export default {
       list: [],
       timer: null
     }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.changeCity(city)
+      this.$router.push('/')
+    },
+    ...mapMutations(['changeCity'])
   },
   computed: {
     hasNoData () {
---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  actions: {
+    changeCity (ctx, city) {
+      console.log(ctx)
+      ctx.commit('changeCity', city)
+    }
+  },
+  mutations,
+  getters: {
+    doubleCity (state) {
+      return state.city + ' ' + state.city
+    }
+  }
+})
