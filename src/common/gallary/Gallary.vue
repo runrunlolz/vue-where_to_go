59c0fd277c355c375ec117e387cddcfe652db61f
runrunlolz <runnan007@126.com>
2019-03-31 17:19:16 +0800

    banner finish

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container" @click="handleGallaryClick">
+        <div class="wrapper">
+            <swiper :options="swiperOptions" v-for="(item, index) in imgs" :key="index">
+                <swiper-slide>
+                    <img class="gallary-img" :src="item">
+                </swiper-slide>
+                <div class="swiper-pagination"  slot="pagination"></div>
+                <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+                <div class="swiper-button-next" slot="button-next"></div>
+                <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+            </swiper>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: 'swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .container >>> .swiper-pagination
+      overflow: inherit
+    .container
+      z-index: 99
+      display: flex
+      flex-direction: column
+      justify-content: center
+      position: fixed
+      left: 0
+      right: 0
+      top: 0
+      bottom: 0
+      background: #000
+      .wrapper
+        overflow: hidden
+        height: 0
+        width: 100%
+        padding-bottom: 60%
+        .gallary-img
+          width: 100%
+        .swiper-pagination
+          color: #fff
+          bottom: -1rem
+</style>
---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1801/4e/4ee5653be58dcb1a3.water.jpg_600x330_f82a1d80.jpg">
+            <div class="banner-info">
+                <div class="banner-title">大连</div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe663;</span>
+                    39
+                </div>
+            </div>
+        </div>
+        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: []
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner
+      overflow: hidden
+      position: relative
+      height: 0
+      padding-bottom: 55%
+      .banner-img
+        width: 100%
+      .banner-info
+        display: flex
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        line-height: .6rem
+        color: #fff
+        background-image: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+        .banner-title
+          flex: 1
+          font-size: .32rem
+          padding: 0 .2rem
+        .banner-number
+          margin-top: .14rem
+          padding: 0 .4rem
+          height: .32rem
+          line-height: .32rem
+          border-radius: .2rem
+          background: rgba(0,0,0,.8)
+          font-size: .24rem
+          .banner-icon
+            font-size: .24rem
+</style>
